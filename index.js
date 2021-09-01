7e287c95b8d1e50685b55b7a79cbb008974a9cad
AbhijeetNandvikar <abhijeetvn1234@gmail.com>
2021-09-01 20:58:05 +0530

    initialized/tried createElement API

---
+++ index.js
@@ -0,0 +1,5 @@
+const App = () => {
+  return React.createElement("div", {}, "Hello World");
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
---
+++ readme.md
@@ -0,0 +1,1 @@
+## This repo contains code for step by step implementation of react project without using create-react-app.
